e6b619ee99bad53fe8ea57e98fa276209b8fc075
Gurpreet <gurpm91@gmail.com>
2017-10-05 18:19:06 +0000

    student exercise

---
+++ sorting_students.js
@@ -0,0 +1,28 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+  students.sort(function (a, b) {
+  return a.age - b.age;
+  });
+
+
+  students.sort(function(a, b) {
+  var nameA = a.name.toUpperCase(); 
+  var nameB = b.name.toUpperCase();
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+});
+
+
+console.log(students)
