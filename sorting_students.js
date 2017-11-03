fc8287fbb966ac37c4de0613bf6bddb9bd9195c1
gurpm1991 <gurpm91@gmail.com>
2017-11-03 13:00:48 +0000

    add assignment

--- sorting_students.js
+++ sorting_students.js
@@ -5,24 +5,22 @@ var students = [
   { id: 4, name: "alex",     age: 30 }
 ];
 
-  students.sort(function (a, b) {
-  return a.age - b.age;
-  });
+
+students.sort(function(a,b){
+  return a.age - b.age
+})
 
 
-  students.sort(function(a, b) {
-  var nameA = a.name.toUpperCase(); 
-  var nameB = b.name.toUpperCase();
-  if (nameA < nameB) {
+students.sort(function(a,b){
+  var nameA = a.name.toUpperCase;
+  var nameB = b.name.toUpperCase;
+  if (nameA < nameB){
     return -1;
   }
-  if (nameA > nameB) {
+  if (nameA > nameB){
     return 1;
   }
+    return 0;
+})
 
-  // names must be equal
-  return 0;
-});
-
-
-console.log(students)
+console.log(students);
